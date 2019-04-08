67a28b60b3438ca57df010e996dfa3f030506f51
dxp2020 <364499854@qq.cm>
2019-04-09 00:27:46 +0800

    初始导入

---
+++ app/src/main/java/com/changf/baolvmd/activity/SettingActivity.java
@@ -0,0 +1,46 @@
+package com.changf.baolvmd.activity;
+
+import android.view.View;
+import android.widget.ListView;
+
+import com.changf.baolvmd.R;
+import com.changf.baolvmd.activity.util.FileUtil;
+import com.mula.base.activity.BaseActivity;
+import com.mula.base.tools.display.ToastUtil;
+
+import java.util.List;
+
+import butterknife.BindView;
+
+public class SettingActivity extends BaseActivity {
+    @BindView(R.id.lv_list)
+    ListView lvList;
+    List<String> goods;
+
+    public void onImportFile(View view) {
+        goods = FileUtil.getFromAssets(mActivity,"goods.txt");
+        if(goods.size()>0){
+            ToastUtil.show("导入成功："+goods.size()+"个");
+        }else{
+            ToastUtil.show("导入失败");
+        }
+    }
+
+    public void addMonster(View view) {
+
+    }
+
+    public void addEquipment(View view) {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.layout_setting;
+    }
+}
