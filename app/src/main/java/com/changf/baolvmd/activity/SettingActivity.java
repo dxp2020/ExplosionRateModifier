26e3e52fa67826feb9dfe0e47e67d95cdf8c83e1
duxiaopan <>
2019-04-09 14:08:53 +0800

    添加怪物

--- app/src/main/java/com/changf/baolvmd/activity/SettingActivity.java
+++ app/src/main/java/com/changf/baolvmd/activity/SettingActivity.java
@@ -1,46 +1,73 @@
 package com.changf.baolvmd.activity;
 
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.changf.baolvmd.R;
 import com.changf.baolvmd.util.FileUtil;
 import com.mula.base.activity.BaseActivity;
 import com.mula.base.tools.display.ToastUtil;
+import com.mula.base.view.MulaTitleBar;
 
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SettingActivity extends BaseActivity {
+    @BindView(R.id.tv_goods_content)
+    TextView tvGoodsContent;
+    @BindView(R.id.btn_check_goods)
+    Button btnCheckGoods;
+    @BindView(R.id.ll_goods)
+    LinearLayout llGoods;
+    @BindView(R.id.tv_monster_content)
+    TextView tvMonsterContent;
+    @BindView(R.id.btn_check_monster)
+    Button btnCheckMonster;
+    @BindView(R.id.ll_monster)
+    LinearLayout llMonster;
+    @BindView(R.id.ll_content)
+    LinearLayout llContent;
     @BindView(R.id.lv_list)
     ListView lvList;
     List<String> goods;
+    List<String> monster;
+    @BindView(R.id.mtb_title_bar)
+    MulaTitleBar mtbTitleBar;
 
     public void onImportFile(View view) {
-        goods = FileUtil.getFromAssets(mActivity,"goods.txt");
-        if(goods.size()>0){
-            ToastUtil.show("导入成功："+goods.size()+"个");
-        }else{
+        goods = FileUtil.getFromAssets(mActivity, "goods.txt");
+        monster = FileUtil.getFromAssets(mActivity, "MonItems");
+        if (goods.size() > 0 || monster.size() > 0) {
+            ToastUtil.show("导入成功");
+        } else {
             ToastUtil.show("导入失败");
         }
-    }
-
-    public void addMonster(View view) {
-
-    }
-
-    public void addEquipment(View view) {
-
+        if (goods.size() > 0) {
+            tvGoodsContent.setText("共添加物品" + goods.size() + "件");
+            llGoods.setVisibility(View.VISIBLE);
+        }
+        if (monster.size() > 0) {
+            tvMonsterContent.setText("共添加怪物" + monster.size() + "个");
+            llMonster.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
     protected void initView() {
-
+        mtbTitleBar.setTitle("爆率修改器");
+        mtbTitleBar.getBackImage().setVisibility(View.GONE);
+        lvList.setEmptyView(llContent);
     }
 
     @Override
     public int getLayoutId() {
         return R.layout.layout_setting;
     }
+
 }
--- app/src/main/java/com/changf/baolvmd/util/FileUtil.java
+++ app/src/main/java/com/changf/baolvmd/util/FileUtil.java
@@ -7,22 +7,32 @@ import com.mula.base.tools.L;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class FileUtil {
 
     public static List<String> getFromAssets(Context context, String fileName){
         List<String> list = new ArrayList<>();
-        try {
-            BufferedReader bufReader = new BufferedReader(new InputStreamReader(context.getResources().getAssets().open(fileName)));
-            String line;
-            while((line = bufReader.readLine()) != null){
-                if(!"".equals(line)){
-                    list.add(line);
+        if ("goods.txt".equals(fileName)) {
+            try {
+                BufferedReader bufReader = new BufferedReader(new InputStreamReader(context.getResources().getAssets().open(fileName)));
+                String line;
+                while((line = bufReader.readLine()) != null){
+                    if(!"".equals(line)){
+                        list.add(line);
+                    }
                 }
+            } catch (Exception e) {
+                L.e(e);
             }
-        } catch (Exception e) {
-            L.e(e);
+        } else {
+            try {
+                String[]listFiles=context.getAssets().list(fileName);
+                Collections.addAll(list, listFiles);
+            } catch (Exception e) {
+                L.e(e);
+            }
         }
         return list;
     }
